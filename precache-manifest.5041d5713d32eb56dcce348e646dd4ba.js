c7915cdc7cb3759350f97e5ec9f2f8aea4d62ed3
oodles-chaddha <59246813+oodles-chaddha@users.noreply.github.com>
2020-09-19 20:08:45 +0530

    Updates

---
+++ precache-manifest.5041d5713d32eb56dcce348e646dd4ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f1662265b844477aec62409e4e0e19",
+    "url": "/comparer/index.html"
+  },
+  {
+    "revision": "38cb0f223c5df0ee82d4",
+    "url": "/comparer/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "bc75e7129fdb8ed65c11",
+    "url": "/comparer/static/css/main.c345f299.chunk.css"
+  },
+  {
+    "revision": "38cb0f223c5df0ee82d4",
+    "url": "/comparer/static/js/2.0af3ad33.chunk.js"
+  },
+  {
+    "revision": "6def8d787cc4234a4738775789db8a47",
+    "url": "/comparer/static/js/2.0af3ad33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc75e7129fdb8ed65c11",
+    "url": "/comparer/static/js/main.f49f7a90.chunk.js"
+  },
+  {
+    "revision": "ff6c41361141ca1307fc",
+    "url": "/comparer/static/js/runtime-main.4098ea0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/comparer/static/media/logo.5d5d9eef.svg"
+  }
+]);
